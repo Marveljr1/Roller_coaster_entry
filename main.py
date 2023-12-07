a571fcf2c78f91ca09735ffd806099d885fa103a
Keith Williams <kwilliams623@hotmail.com>
2023-12-07 01:30:37 -0500

    updated the billing

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 print("Welcome to the Rollercoaster!!!!")
 height = int(input("What is your height in cm?"))
+bill = 0
 
 if height >=  120:
     print("You can ride the Rollercoaster.")
     age = int(input("What is your age \n"))
     if age < 12:
-        print("Please pay $5.00 ")
+        bill = 5
+        print("Children tickets are $5.00 ")
     elif age <= 18:
-        print("Please pay $7.00")
+        bill = 7
+        print("Youth tickets are $7.00")
     else:
-        print("Please pay $12.00")
+        bill = 12
+        print("Adult tickets are $12.00")
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
+
 else:
     print("You are too short to ride the rollercoaster.")
